7f98a355ad2a91ddd6888c819d169a0fb1fbd686
Diego <diegoramos3709@gmail.com>
2025-03-18 16:55:51 -0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 //Solve cors
-app.use(cors({credentials: true, origin: "http://localhost:5173"}))
+app.use(cors({credentials: true, origin: "https://projeto-reactgram.vercel.app/"}))
 
 //Upload directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
